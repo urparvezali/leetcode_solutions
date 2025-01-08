289e5cac828158844b84cfe58a0813f83688603f
Parvez Ali <85557535+urparvezali@users.noreply.github.com>
2025-01-08 23:23:48 +0600

    Time: 21 ms (52.78%) | Memory: 2.4 MB (27.78%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut dp = vec![1; n];
+        let mut mx = 0;
+        for i in (0..n).rev() {
+            mx = 0;
+            for j in i + 1..n {
+                if nums[i] < nums[j] {
+                    mx = mx.max(dp[j]);
+                }
+            }
+            dp[i] += mx;
+        }
+        *dp.iter().max().unwrap()
+    }
+}
